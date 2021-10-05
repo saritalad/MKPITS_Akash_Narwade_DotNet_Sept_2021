778aba2707d0e61e2bedb103f7def4fe5605475a
akash narwade <narwadeakash.2108@gmail.com>
2021-10-05 09:03:18 +0530

    ass4 ass5 ass6 ass7 ass8

---
+++ C_Programs/6assg.c
@@ -0,0 +1,14 @@
+/* Sixth Assignment */
+/* pre processor directive */
+#include<stdio.h>
+
+/* global variable declaration */
+int main()
+{
+
+printf(" \n Akash Narwade \n Akash Narwade \n Akash Narwade \n Akash Narwade\n Akash Narwade \n Akash Narwade \n Akash Narwade \n Akash Narwade \n Akash Narwade \n Akash Narwade");
+
+return 0;
+
+}
+
